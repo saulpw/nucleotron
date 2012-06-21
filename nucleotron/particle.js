5a3ec3885db32b12ee48f43ccbc9d106f3706f19
Leo Burke <burke.leo@gmail.com>
2012-06-21 17:17:55 -0500

    added particle.js

--- nucleotron/particle.js
+++ nucleotron/particle.js
@@ -7,6 +7,9 @@ goog.require('lime.Circle');
 nucleotron.Particle = function(){
 	lime.Sprite.call(this);
 	this.setSize(10,10);
+	this.RADIUS = 10;
+    this.SPEED = .45;
+	this.ball = new lime.Circle().setSize(this.RADIUS * 2, this.RADIUS * 2).setFill(200, 0, 0);
 	
-}
+}	
 goog.inherits(nucleotron.Particle, lime.Sprite);
